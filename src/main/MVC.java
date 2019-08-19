bf0bc962ae86d4018eaffacd2353603abff7bae3
Pipe <felipegguzman19@gmail.com>
2019-08-19 09:42:30 -0500

    lector de archivos en model.logic ln43

--- src/main/MVC.java
+++ src/main/MVC.java
@@ -1,10 +1,10 @@
 package main;
+
 import controller.Controller;
 
 public class MVC {
-	
-	public static void main(String[] args) 
-	{
+
+	public static void main(String[] args) {
 		Controller controler = new Controller();
 		controler.run();
 	}
